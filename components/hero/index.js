8f8ef0940906d8b99e9166b5f50974efbfb2b2d0
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-09 16:08:30 +0530

    Delete components directory

--- components/hero/index.js
+++
@@ -1,6 +0,0 @@
-import Image from 'next/image'
-
-export default ()=><section className="hero-section">
-<img style={{ width: '100%', height: '60vh',objectFit:"cover" ,objectPosition:"0% 13%"}} 
-     src="/media/hero/company.png" />
-</section>
