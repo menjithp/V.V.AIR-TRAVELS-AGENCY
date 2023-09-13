a2beb690da6a45ce33e77eb329008663a7038509
Menjith P <70083973+menjithp@users.noreply.github.com>
2023-09-13 17:08:50 +0530

    Delete Basic-components directory

--- Basic-components/loader.js
+++
@@ -1,8 +0,0 @@
-import style from './loader.module.css'
-
-export default ()=>{
-
-    return <div className={`${style.backgroundloader} d-flex justify-content-center align-items-center position-fixed start-0 end-0 top-0 bottom-0`}>
-        <span className={style.loader}></span>
-    </div>
-}
